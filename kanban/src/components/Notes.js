027bbca1aef4272254a795072f7fcbc78f0ae9af
Juho Vepsalainen <bebraw@gmail.com>
2016-09-19 14:04:11 +0300

    Notes - Rewrite in arrow notation
    Not as fast as it could since it creates extra functions on `render()`,
    but given `Notes` isn't performance sensitive, it doesn't really matter.

--- kanban/src/components/Notes.js
+++ kanban/src/components/Notes.js
@@ -7,23 +7,25 @@ const Notes = ({
   notes,
   onNoteClick=() => {}, onEdit=() => {}, onDelete=() => {}
 }) => (
-  <ul className="notes">{notes.map(({id, editing, task}) =>
-    <li key={id}>
-      <Note className="note" id={id}
-        editing={editing}
-        onClick={onNoteClick.bind(null, id)}
-        onMove={LaneActions.move}>
-        <Editable
-          className="editable"
+  <ul className="notes">{
+    notes.map(({id, editing, task}) =>
+      <li key={id}>
+        <Note className="note" id={id}
           editing={editing}
-          value={task}
-          onEdit={onEdit.bind(null, id)} />
-        <button
-          className="delete"
-          onClick={onDelete.bind(null, id)}>x</button>
-      </Note>
-    </li>
-  )}</ul>
+          onClick={e => onNoteClick(id, e)}
+          onMove={LaneActions.move}>
+          <Editable
+            className="editable"
+            editing={editing}
+            value={task}
+            onEdit={e => onEdit(id, e)} />
+          <button
+            className="delete"
+            onClick={e => onDelete(id, e)}>x</button>
+        </Note>
+      </li>
+    )
+  }</ul>
 );
 
 export default Notes;
