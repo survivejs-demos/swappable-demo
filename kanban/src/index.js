d7b216437f45732330027ca1fba06fdc98e99cd8
Juho Vepsalainen <bebraw@gmail.com>
2016-09-19 13:37:38 +0300

    Import styling

--- kanban/src/index.js
+++ kanban/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Provider from './components/Provider';
+import './main.css';
 
 ReactDOM.render(
   <Provider><App /></Provider>,
