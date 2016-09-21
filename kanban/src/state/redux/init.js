87949a4fc05577196bb199c948af2b8fee041fc1
Juho Vepsalainen <bebraw@gmail.com>
2016-09-21 09:56:15 +0300

    redux - Fix init
    Still need to add a swap control somewhere.

--- kanban/src/state/redux/init.js
+++ kanban/src/state/redux/init.js
@@ -5,14 +5,13 @@ export default () => {
   const appStorage = 'redux-app';
   const storageTarget = localStorage;
 
-  // XXXXX: debug
-  /*
-  const store = reduxStore.init(storage(storageTarget).get(appStorage));
+  const store = reduxStore.init(
+    storage(storageTarget).get(appStorage) || {}
+  );
 
   store.subscribe(() => {
     if (!storage(storageTarget).get('debug')) {
       storage(storageTarget).set(appStorage, store.getState());
     }
   });
-  */
 };
